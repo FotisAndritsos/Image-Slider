ceba72187990a901dd23f1aab415035248465aaa
FotisAndritsos <fotisandritsos85@gmail.com>
2020-10-14 22:26:52 +0300

    js done

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-const right = document.getElementById('right');
-const left = document.getElementById('left');
-const btn = document.querySelectorAll('button');
-const sld = document.querySelector('.sldContainer');
-const imgs = document.querySelectorAll('.imgs');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const niaou = document.querySelector('.sldContainer');
+const imgs = document.querySelectorAll('.sldContainer img');
 
 
-let inx = 0;
+let  counter = 1;
+const size = imgs[0].clientWidth;
+
+niaou.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 
-btn.forEach(buttons =>{
-    buttons.addEventListener('click',function(){
-        if (buttons.id == 'right'){
-            inx++;
-            imgs.style.transform = `translateX(${-inx * 600}px)`;
-        }else if (buttons.id == 'left'){
-            inx--;
-            imgs.style.transform = `translateX(${inx * 600}px)`; 
-        }
-    })
+nextBtn.addEventListener('click', function () {
+    if(counter >= imgs.length -1) return;
+    niaou.style.transition = 'transform 0.5s ease-in-out'
+    counter++;
+    niaou.style.transform = 'translateX(' + (-size * counter) + 'px)';
+})
+prevBtn.addEventListener('click', function () {
+    if(counter <= 0) return;
+    niaou.style.transition = 'transform 0.5s ease-in-out'
+    counter--;
+    niaou.style.transform = 'translateX(' + (-size * counter) + 'px)';
 })
 
+niaou.addEventListener('transitionend', () => {
+    if (imgs[counter].id === 'lastClone'){
+        niaou.style.transition = "none";
+        counter = imgs.length - 2;
+        niaou.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (imgs[counter].id === 'firstClone'){
+        niaou.style.transition = "none";
+        counter = imgs.length - counter;
+        niaou.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+})
